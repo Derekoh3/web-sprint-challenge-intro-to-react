c300be6153a4f340e2818c9a809d9f06eb984895
Derek Oh <D.O@Dereks-Air.fios-router.home>
2021-07-16 13:04:39 -0400

    pushing changes

--- src/App.js
+++ src/App.js
@@ -1,19 +1,33 @@
-import React from 'react';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import axios from "axios";
+import { CHAR_URL } from "./index";
+import Character from "./components/Character";
 
 const App = () => {
-  // Try to think through what state you'll need for this app before starting. Then build out
-  // the state properties here.
+  const [charData, setCharData] = useState([]);
 
-  // Fetch characters from the API in an effect hook. Remember, anytime you have a 
-  // side effect in a component, you want to think about which state and/or props it should
-  // sync up with, if any.
+  const fetchChars = () => {
+    axios
+      .get(CHAR_URL)
+      .then((res) => {
+        setCharData(res.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
+  useEffect(fetchChars, []);
 
   return (
     <div className="App">
       <h1 className="Header">Characters</h1>
+      {charData.map((charArray) => {
+        return <Character key={charArray.name} charData={charArray} />;
+      })}
     </div>
   );
-}
+};
 
 export default App;
